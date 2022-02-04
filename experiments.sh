d6a4daf1667f3f014eacf19003831331246b1e52
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 19:27:16 +0100

    "Experiments"

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#0036593459
 mkdir -p outputs/Example2/Markdown
 bsub -o "outputs/Example2/Markdown/Example2_0.md" -J "Example2_0" -env MYARGS="-name Example2-0 -GPU True -time 3600 -b 4.0 -a 1 -d dssf -ID 0" < submit_gpu.sh
 mkdir -p outputs/Example3/Markdown
