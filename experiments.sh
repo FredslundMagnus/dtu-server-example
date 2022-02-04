2761195e979e613f641414fec41a39bf91c959d3
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 18:07:35 +0100

    "Experiments"

--- experiments.sh
+++ experiments.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-#3800464078
-mkdir ../outputs/Example1/
-mkdir ../outputs/Example1/Markdown
-bsub -o "../outputs/Example1/Markdown/Example1_0.md" -J "Example1_0" -env MYARGS="-name Example1-0 -GPU False -time 3600 -b 4.0 -a 1 -d dsf -ID 0" < submit_cpu.sh
-mkdir ../outputs/Example2/
-mkdir ../outputs/Example2/Markdown
-bsub -o "../outputs/Example2/Markdown/Example2_0.md" -J "Example2_0" -env MYARGS="-name Example2-0 -GPU True -time 3600 -b 4.0 -a 1 -d dssf -ID 0" < submit_gpu.sh
-mkdir ../outputs/Example3/
-mkdir ../outputs/Example3/Markdown
-bsub -o "../outputs/Example3/Markdown/Example3_0.md" -J "Example3_0" -env MYARGS="-name Example3-0 -GPU False -time 3600 -b 2.0 -a 2 -d fd -ID 0" < submit_cpu.sh
-bsub -o "../outputs/Example3/Markdown/Example3_1.md" -J "Example3_1" -env MYARGS="-name Example3-1 -GPU False -time 3600 -b 2.0 -a 2 -d fd -ID 1" < submit_cpu.sh
+#7350012501
+mkdir -p outputs/Example1/Markdown
+bsub -o "outputs/Example1/Markdown/Example1_0.md" -J "Example1_0" -env MYARGS="-name Example1-0 -GPU False -time 3600 -b 4.0 -a 1 -d dsf -ID 0" < submit_cpu.sh
+mkdir -p outputs/Example2/Markdown
+bsub -o "outputs/Example2/Markdown/Example2_0.md" -J "Example2_0" -env MYARGS="-name Example2-0 -GPU True -time 3600 -b 4.0 -a 1 -d dssf -ID 0" < submit_gpu.sh
+mkdir -p outputs/Example3/Markdown
+bsub -o "outputs/Example3/Markdown/Example3_0.md" -J "Example3_0" -env MYARGS="-name Example3-0 -GPU False -time 3600 -b 2.0 -a 2 -d fd -ID 0" < submit_cpu.sh
+bsub -o "outputs/Example3/Markdown/Example3_1.md" -J "Example3_1" -env MYARGS="-name Example3-1 -GPU False -time 3600 -b 2.0 -a 2 -d fd -ID 1" < submit_cpu.sh
